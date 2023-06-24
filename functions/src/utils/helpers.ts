0bc55e3f1995886d9c998f7b1036e3b5b313d594
Alvin Bellero <ajames.bellero@gmail.com>
2023-06-25 00:41:39 +0800

    :art: General update

--- functions/src/utils/helpers.ts
+++ functions/src/utils/helpers.ts
@@ -19,30 +19,3 @@ export const generateCompletionParams = (
     ...(topP ? { top_p: topP } : {}),
   };
 };
-
-/**
- * Sanitizes the provided path by removing any leading or trailing slashes.
- *
- * @param {string} path - The Firestore path to sanitize.
- * @returns {string} - The sanitized path.
- */
-export const sanitizePath = (path: string): string => {
-  return path.replace(/^\/|\/$/g, "");
-};
-
-/**
- * Determines whether the provided path is a valid Firestore collection.
- * A path is considered a valid collection if it has an even number of path segments
- * after removing any leading or trailing slashes and filtering out any empty segments.
- *
- * @param {string} path - The Firestore path to validate.
- * @returns {[boolean, string]} - A tuple where the first element indicates whether the path is a valid collection
- * and the second element is the sanitized path if it is valid, or an empty string if it is not.
- */
-export const isValidCollection = (path: string): [boolean, string] => {
-  if (!path) return [false, ""];
-  const sanitizedPath = sanitizePath(path);
-  const pathSegments = sanitizedPath.split("/").filter(Boolean);
-  const isValid = pathSegments.length % 2 !== 0;
-  return [isValid, isValid ? sanitizedPath : ""];
-};
